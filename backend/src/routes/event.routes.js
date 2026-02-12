0d531715c16d5b96db35378e34f45a5cd06e24cc
Farhan <farhanpoint2000@gmail.com>
2026-02-12 22:03:57 +0530

    updated event.controller.js

--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
 
 const authenticate = require("../middleware/auth.middleware");
 const authorizeAdmin = require("../middleware/authorizeadmin");
