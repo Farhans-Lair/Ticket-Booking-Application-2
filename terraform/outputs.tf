09bd69cdb17909ec33f18aefcef63cfb4647daf2
Farhan <farhanpoint2000@gmail.com>
2026-03-31 22:32:53 +0530

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,18 @@ output "rds_endpoint" {
 output "alb_dns_name" {
   value = aws_lb.ticket_alb.dns_name
 }
+
+output "app_url" {
+  description = "Open this URL in your browser (click through the self-signed cert warning once)"
+  value       = "https://${aws_lb.ticket_alb.dns_name}"
+}
+
+output "iam_certificate_name" {
+  description = "Name of the self-signed cert uploaded to IAM"
+  value       = aws_iam_server_certificate.self_signed.name
+}
+
+output "iam_certificate_arn" {
+  description = "ARN of the IAM cert attached to the ALB HTTPS listener"
+  value       = aws_iam_server_certificate.self_signed.arn
+}
